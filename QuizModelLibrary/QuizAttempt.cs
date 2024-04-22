bb015a34dd8da8d6705eb608c3e223a53143b4be
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-04-22 15:28:15 +0530

    Add QuizAttempt Prop and ToString

--- QuizModelLibrary/QuizAttempt.cs
+++ QuizModelLibrary/QuizAttempt.cs
@@ -1,12 +1,18 @@
 ﻿namespace QuizModelLibrary
 {
-    public class QuizAttempt(int quizId, int userId)
+    public class QuizAttempt(int quizId, int userId, int score, List<QuizQuestion> correctlyAnswered, List<QuizQuestion> wrongQuestions)
     {
         public int Id { get; set; }
 
         public int QuizId { get; set; } = quizId;
 
         public int UserId { get; set; } = userId;
+
+        public int Score { get; set; } = score;
+
+        public List<QuizQuestion> CorrectlyAnswered { get; set; } = correctlyAnswered;
+
+        public List<QuizQuestion> WronglyAnswered { get; set; } = wrongQuestions;
 
         public override int GetHashCode()
         {
@@ -15,7 +21,23 @@
 
         public override string ToString()
         {
-            return base.ToString();
+            string output = $"Attempt ID - {Id}\nQuiz ID {QuizId}\nUser Id {UserId}\nScore {Score}\n";
+
+            output += "Correctly Answered\n";
+
+            foreach (var question in CorrectlyAnswered)
+            {
+                output += question + "\n";
+            }
+
+            output += "Wrongly Answered\n";
+
+            foreach (var question in WronglyAnswered)
+            {
+                output += question + "\n";
+            }
+
+            return output;
         }
 
         public override bool Equals(object? obj)
