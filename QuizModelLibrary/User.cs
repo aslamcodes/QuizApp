0fb8ee197be3fb7d0bee3e0d75ee011758b2a613
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-04-22 12:14:34 +0530

    Introduce Password Propertiey to user Model

--- QuizModelLibrary/User.cs
+++ QuizModelLibrary/User.cs
@@ -1,11 +1,13 @@
 ﻿namespace QuizModelLibrary
 {
-    public class User(string name)
+    public class User(string name, string password)
     {
 
         public int Id { get; set; }
 
         public string Name { get; set; } = name;
+
+        public string Password { get; set; } = password;
 
         public override int GetHashCode()
         {
